329fdb2f23d10facb83e075eeb50930d8a34fa7a
Tan Patipan Khamwachirapitak <tanpkh97@gmail.com>
2026-09-16 16:20:46 +0700

    Overbleed fix 2

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'vasilina-portfolio-v118';
+const CACHE_NAME = 'vasilina-portfolio-v119';
 
 // Core files to cache instantly when the site first loads
 const CORE_ASSETS = [
     '/manifest.json',
-    '/style.css?v=118',
+    '/style.css?v=119',
     '/main.js?v=115',
     '/image-captions.js?v=92',
     '/nav.js?v=116',
